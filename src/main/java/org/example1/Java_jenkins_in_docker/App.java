9c1f53294de61ba81eaf46b5a5d63e133418f36b
bittu <19ramshri@gmail.com>
2022-01-09 10:58:03 +0530

    punit

---
+++ src/main/java/org/example1/Java_jenkins_in_docker/App.java
@@ -0,0 +1,19 @@
+package org.example1.Java_jenkins_in_docker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+        public static void main(String[] args) {
+            System.out.println(checkIfInputIsAnEvenNumber(122)); // Testing in the main method
+        }
+    
+        public static boolean checkIfInputIsAnEvenNumber(int number){
+            return number % 2 == 0;
+        }
+    
+
+}
